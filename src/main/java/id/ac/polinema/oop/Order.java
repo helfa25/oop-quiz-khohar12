658a5f05bc2b26b1a0a6f544447f5779e2493080
helfa25 <calony64@gmail.com>
2026-09-25 09:37:23 +0700

    done percobaan

--- src/main/java/id/ac/polinema/oop/Order.java
+++ src/main/java/id/ac/polinema/oop/Order.java
@@ -39,7 +39,7 @@ public class Order {
     public double getFinalTotal() {
         double total = getTotal();
         if (total >= 100000.0) { 
-            return total * 1.0;
+            return total * 0.9;
         }
         return total;
     }
